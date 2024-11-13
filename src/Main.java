d2aef61d1ddfa78c98d4afd98cb74a110ba76614
ABDULLAZADEH-NASIR <161652810+ABDULLAZADEH-NASIR@users.noreply.github.com>
2024-11-13 19:22:03 +0400

    Update Main.java
    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Duck duck1 = new MallardDuck();
+        Duck duck2 = new RubberDuck();
+        Duck duck3 = new ReadHeadDuck();
+        Duck duck4 = new DecoyDuck();
+
+        duck1.display();
+        duck2.display();
+        duck3.display();
+        duck4.display();
+
+        duck1.swim();
+        duck2.swim();
+        duck3.swim();
+
+
     }
+
 }

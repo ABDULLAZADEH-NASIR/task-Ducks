990bca760fa3e7bd353cb9de3e790b39f0c6970d
ABDULLAZADEH-NASIR <161652810+ABDULLAZADEH-NASIR@users.noreply.github.com>
2024-11-13 20:44:11 +0400

    Update MallardDuck.java
    update project

--- src/MallardDuck.java
+++ src/MallardDuck.java
@@ -8,11 +8,11 @@ public class MallardDuck extends Duck implements Flyable,Quackable{
 
     @Override
     public void fly() {
-        System.out.println("Mallar Ducks can fly");
+        System.out.println("Mallard Ducks can fly");
     }
 
     @Override
     public void quack() {
-        System.out.println("Mallar Ducks are quack");
+        System.out.println("Mallard Ducks are quack");
     }
 }
